60f18b5046153151da50082b21822fd9f6627a45
arquitecturabenito <arquitecturabenito@gmail.com>
2025-10-23 17:36:38 +0200

    a

--- pages/ProjectDetailPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// This component is no longer used and has been replaced by static HTML pages
-// for each project. It can be safely deleted.
-const ProjectDetailPage: React.FC = () => {
-  return null;
-};
-
-export default ProjectDetailPage;
